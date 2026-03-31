f95f5c806ce3645a7a541941e2b4c31ad7ad6080
Irine7 <irine7@yandex.ru>
2026-03-31 20:41:27 +0200

    refactor: migrate project to Next.js by replacing express with a proxy implementation and updating configuration

--- next.config.js
+++ next.config.js
@@ -6,4 +6,4 @@ const nextConfig = {
   serverExternalPackages: ['node-cron'], 
 }
 
-module.exports = nextConfig
+export default nextConfig;
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
 	const method = req.method; // Получаем метод запроса
 	const url = req.nextUrl.pathname; // Получаем путь запроса
 	console.log(`🚀 [Middleware Log]: ${method} ${url}`);
